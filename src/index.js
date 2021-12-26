1e56a68a0a454caf721cc540d27f31afcc6599d8
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-12-26 11:01:20 +0300

    reducers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import navbar from './components/navbar';
 import './index.css';
+import store from "./reducers";
 import App from './App';
 
   ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
     <navBar />
       <App />
     </BrowserRouter>,
+    </Provider>,
+
     document.getElementById("root")
   );
   
